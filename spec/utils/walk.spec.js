f3e81cd2f2d054f42c2cdbace9c15cc88cef8d45
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-16 21:48:10 +0000

    refact(walk): rename for-each-recursive to walk

--- spec/utils/walk.spec.js
+++ spec/utils/walk.spec.js
@@ -1,4 +1,4 @@
-var forEachRecursive = require('../../lib/utils/for-each-recursive');
+var walk = require('../../lib/utils/walk');
 var _ = require('lodash');
 
 function toUpperCase(prop) {
@@ -14,21 +14,21 @@ function deleteBad(prop, key) {
   }
 }
 
-describe("for-each-recursive", function() {
+describe("walk", function() {
   it("should apply the function to all properties on an object", function() {
-    expect(forEachRecursive({ a:'a', b:'b', c:'c'}, toUpperCase)).toEqual({a:'A', b:'B', c:'C'});
+    expect(walk({ a:'a', b:'b', c:'c'}, toUpperCase)).toEqual({a:'A', b:'B', c:'C'});
   });
   it("should apply the function to all items in an array", function() {
-    expect(forEachRecursive(['a','b','c'], toUpperCase)).toEqual(['A', 'B', 'C']);
+    expect(walk(['a','b','c'], toUpperCase)).toEqual(['A', 'B', 'C']);
   });
   it("should apply the function to all items in a nested array", function() {
-    expect(forEachRecursive([['a','b','c']], toUpperCase)).toEqual([['A', 'B', 'C']]);
+    expect(walk([['a','b','c']], toUpperCase)).toEqual([['A', 'B', 'C']]);
   });
   it("should apply the function to all items in a nested object", function() {
-    expect(forEachRecursive({ x: { a:'a', b:'b', c:'c'}}, toUpperCase)).toEqual({x:{a:'A', b:'B', c:'C'}});
+    expect(walk({ x: { a:'a', b:'b', c:'c'}}, toUpperCase)).toEqual({x:{a:'A', b:'B', c:'C'}});
   });
   it("should apply the function to all properties recursively", function() {
-    expect(forEachRecursive([
+    expect(walk([
       { a: 'a', b: [ 'x', 'y', 'z'], c: { i: 'i', j:'j', k:'k'}, d: [ [ { m: [ { n: 'o' }]}]]}
     ], toUpperCase)).toEqual([
       { a: 'A', b: [ 'X', 'Y', 'Z'], c: { i: 'I', j:'J', k:'K'}, d: [ [ { m: [ { n: 'O' }]}]]}
@@ -36,14 +36,14 @@ describe("for-each-recursive", function() {
   });
 
   it("should provide the key to the handler function", function() {
-    expect(forEachRecursive({ a: 'a', b:'b', 'bad': 'xxx'}, deleteBad)).toEqual({ a: 'a', b:'b' });
+    expect(walk({ a: 'a', b:'b', 'bad': 'xxx'}, deleteBad)).toEqual({ a: 'a', b:'b' });
   });
 
   it("should apply the function to properties that are arrays", function() {
-    expect(forEachRecursive({ a: ['a'], 'bad': ['xxx']}, deleteBad)).toEqual({ a: ['a']});
+    expect(walk({ a: ['a'], 'bad': ['xxx']}, deleteBad)).toEqual({ a: ['a']});
   });
 
   it("should apply the function to properties that are objects", function() {
-    expect(forEachRecursive({ a: ['a'], 'bad': { x: 'xxx'} }, deleteBad)).toEqual({ a: ['a']});
+    expect(walk({ a: ['a'], 'bad': { x: 'xxx'} }, deleteBad)).toEqual({ a: ['a']});
   });
 });
