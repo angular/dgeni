c696a264cc1d9f6ebf27bef33bb0c050e52d102c
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-16 19:09:06 +0000

    update example with marked markdown processing and other stuff

--- bin/lib/default-setup.js
+++ bin/lib/default-setup.js
@@ -3,7 +3,6 @@ var _ = require('lodash');
 // Extracting docs from files
 var fileReaderFactory = require('../../lib/doc-extractor');
 var docExtractors = require('../../lib/doc-extractor/doc-extractors');
-var readFiles = fileReaderFactory(docExtractors);
 
 // Processing ngdoc tags in extracted docs
 var ngDocProcessorFactory =  require('../../lib/ngdoc-parser');
@@ -11,21 +10,17 @@ var ngdocTagHandlers = require('../../lib/ngdoc-parser/ngdoc-tags');
 var inlineTagHandlers = require('../../lib/ngdoc-parser/ngdoc-tags/inline');
 var ngdocPlugins = require('../../lib/ngdoc-parser/ngdoc-plugins');
 var parseDoc = ngDocProcessorFactory(ngdocTagHandlers, inlineTagHandlers, ngdocPlugins);
-var parseDocs = function(docs) {
-  _.forEach(docs, function(doc) {
-    parseDoc(doc);
-  });
-  return docs;
-};
 
-// Additional processing of extracted docs
-var mergeDocs = require('../../lib/doc-processor/doc-processor-plugins/doc-merger');
+// Doc processor plugins
+var docProcessorPlugins = [
+  require('../../lib/doc-processor/doc-processor-plugins/ngdoc-parser')(parseDoc),
+  require('../../lib/doc-processor/doc-processor-plugins/doc-merger')
+];
 
 // Doc processing
 var docParserFactory = require('../../lib/doc-processor');
-var processDocs = docParserFactory([parseDocs, mergeDocs]);
 
 module.exports = {
-  readFiles: readFiles,
-  processDocs: processDocs
+  readFiles: fileReaderFactory(docExtractors),
+  processDocs: docParserFactory(docProcessorPlugins)
 };
--- bin/render-docs.js
+++ bin/render-docs.js
@@ -12,6 +12,7 @@ var renderDoc = docRendererFactory(myArgs.templates, myArgs.output);
 
 // Extra data
 var git = {
+  // TODO: Get this from somewhere
   tag: "v1.2.6-build.1989+sha.b0474cb"
 };
 
@@ -21,7 +22,7 @@ setup.readFiles(filePath).then(function(docs) {
   console.log('Read', docs.length, 'docs');
   docs = setup.processDocs(docs);
   docs.forEach(function(doc) {
-    var renderedFilePath = renderDoc({ doc: doc, git: git });
+    var renderedFilePath = renderDoc({ doc: doc, git: git});
     console.log('Rendered doc', renderedFilePath);
   });
 }).done();
