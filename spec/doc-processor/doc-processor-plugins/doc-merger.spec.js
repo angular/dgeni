57c8306792dd835baa40f5b74dda790e8f1fd3ff
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-16 21:23:05 +0000

    feat(doc-merger): ditch the methodOf, eventOf, etc and parse it from the id

--- spec/doc-processor/doc-processor-plugins/doc-merger.spec.js
+++ spec/doc-processor/doc-processor-plugins/doc-merger.spec.js
@@ -4,26 +4,26 @@ describe('doc-merger', function() {
   var docs, mergedDocs;
   beforeEach(function() {
     docs = [
-      { id: 'x.y.z' },
-      { id: 'a.b.c#prop', propertyOf: 'a.b.c' },
-      { id: 'a.b.c' },
-      { id: 'a.b.c#event', eventOf: 'a.b.c' },
-      { id: 'f.g.h' },
-      { id: 'a.b.c#method', methodOf: 'a.b.c' },
+      { ngdoc: 'service', id: 'x.y.z' },
+      { ngdoc: 'property', id: 'a.b.c#someProp' },
+      { ngdoc: 'service',  id: 'a.b.c' },
+      { ngdoc: 'event', id: 'a.b.c#someEvent' },
+      { ngdoc: 'service',  id: 'f.g.h' },
+      { ngdoc: 'method', id: 'a.b.c#someMethod' },
     ];
     mergedDocs = mergeDocs(docs);
   });
 
   it("should merge property docs into the parent doc", function() {
-    expect(mergedDocs[1].properties).toEqual([{ id: 'a.b.c#prop', propertyOf: 'a.b.c' }]);
+    expect(mergedDocs[1].properties[0].id).toEqual('a.b.c#someProp');
   });
 
   it("should merge method docs into the parent doc", function() {
-    expect(mergedDocs[1].methods).toEqual([{ id: 'a.b.c#method', methodOf: 'a.b.c' }]);
+    expect(mergedDocs[1].methods[0].id).toEqual('a.b.c#someMethod');
   });
 
   it("should merge event docs into the parent doc", function() {
-    expect(mergedDocs[1].events).toEqual([{ id: 'a.b.c#event', eventOf: 'a.b.c' }]);
+    expect(mergedDocs[1].events[0].id).toEqual('a.b.c#someEvent');
   });
 
   it("should remove child docs from the merged docs collection", function() {
