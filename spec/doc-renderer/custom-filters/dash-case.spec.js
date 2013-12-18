f4c2334ff6a623d5050ba820b59745378f6b7081
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-18 15:03:08 +0000

    feat(dashCase): add new dash-case filter

---
+++ spec/doc-renderer/custom-filters/dash-case.spec.js
@@ -0,0 +1,10 @@
+var filter = require('../../../lib/doc-renderer/custom-filters/dash-case');
+
+describe("dashCase custom filter", function() {
+  it("should have the name 'dashCase'", function() {
+    expect(filter.name).toEqual('dashCase');
+  });
+  it("should transform the content to dash-case", function() {
+    expect(filter.process('fooBar')).toEqual('foo-bar');
+  });
+});
