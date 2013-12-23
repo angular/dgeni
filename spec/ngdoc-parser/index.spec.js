479bf8d66fc1e7aae6c50a41c0be6aa58a75a0e3
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-23 15:11:36 +0000

    refact(ngdoc): refactor inline tags to be able to return a function to run later
    We were running inline tags after all tags and plugins had completed. This is necessary because
    some in-line tags need meta-data that is extracted from other tags in order to work; but this meant
    that we had to search the processed doc properties, recursively, for all inline tags, even in the
    original text.
    
    This refactoring allows in-line tag handler to return either a string or a function.  If the handler
    returns a string it will replace the tag in the text. If the handler returns a function the function
    will be stored and then run at the end of the ngdoc processing.  The result of running this function
    will then replace the in-line tag in the text, wherever it appears in the doc.

--- spec/ngdoc-parser/index.spec.js
+++ spec/ngdoc-parser/index.spec.js
@@ -8,8 +8,8 @@ describe("ngdoc", function() {
       if ( doc.log ) { doc.log.push(val); }
     }
 
-    tagHandlerSpy = jasmine.createSpy('tagHandler').andCallFake(function(tag, doc) { log(doc, 'TAG'); return true;});
-    inlineTagHandlerSpy = jasmine.createSpy('inlineTagHandler').andCallFake(function(doc) { log(doc, 'INLINE_TAG'); return 'inline tag';});
+    tagHandlerSpy = jasmine.createSpy('tagHandler').andCallFake(function(tag) { log(tag.doc, 'TAG'); tag.doc.tagContent = tag.text; return true;});
+    inlineTagHandlerSpy = jasmine.createSpy('inlineTagHandler').andCallFake(function(tag) { log(tag.doc, 'INLINE_TAG'); return function getText() { return 'PROCESSED TAG'; };} );
     pluginSpy = jasmine.createSpyObj('pluginSpy', ['before', 'after']);
     pluginSpy.before.andCallFake(function(doc) { log(doc, 'PLUGIN-BEFORE'); });
     pluginSpy.after.andCallFake(function(doc) { log(doc, 'PLUGIN-AFTER'); });
@@ -20,29 +20,48 @@ describe("ngdoc", function() {
   it("should call provided tag handler functions to process tags", function() {
     var doc = { content: 'some text with a\n@tag in it'};
     processDoc(doc);
-    expect(tagHandlerSpy).toHaveBeenCalledWith(
-      { name : 'tag', text : 'in it', lines : ['in it'] }, 
-      doc);
+    expect(tagHandlerSpy).toHaveBeenCalledWith({ doc: doc, name : 'tag', text : 'in it' });
+  });
+
+  it("should split string properties that contain inline tags into arrays", function() {
+    doc = { content: '@tag containing an {@inline inline tag} inside it'};
+    processDoc(doc);
+    expect(doc.tagContent).toEqual('containing an PROCESSED TAG inside it');
   });
 
   it("should call the inline tag handlers if an inline tag is found", function() {
     doc = { content: '@tag containing an {@inline inline tag content} within'};
+    var expectedTag = {
+      doc: doc,
+      name: 'tag',
+      text : jasmine.any(Function)
+    };
     processDoc(doc);
-    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(doc, 'inline', 'inline tag content');
+    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(expectedTag, 'inline', 'inline tag content' );
 
     doc = { content: '@tag containing an {@inline} within'};
+    expectedTag = {
+      doc: doc,
+      name: 'tag',
+      text : jasmine.any(Function)
+    };
     processDoc(doc);
-    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(doc, 'inline', '');
+    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(expectedTag, 'inline', '');
 
     doc = { content: '@tag containing an {@inline `unusual chars`} within'};
+    expectedTag = {
+      doc: doc,
+      name: 'tag',
+      text : jasmine.any(Function)
+    };
     processDoc(doc);
-    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(doc, 'inline', '`unusual chars`');
+    expect(inlineTagHandlerSpy).toHaveBeenCalledWith(expectedTag, 'inline', '`unusual chars`');
   });
 
   it("should call the tag handlers and plugins in the right order", function() {
     doc = { content: '@tag {@inline}', log: [] };
     processDoc(doc);
-    expect(doc.log).toEqual(['PLUGIN-BEFORE', 'TAG', 'PLUGIN-AFTER', 'INLINE_TAG']);
+    expect(doc.log).toEqual(['PLUGIN-BEFORE', 'INLINE_TAG', 'TAG', 'PLUGIN-AFTER']);
   });
 
   it("should call the plugins before and after parsing", function() {
