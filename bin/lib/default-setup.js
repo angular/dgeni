f91d458a11ee14864709af2640333e1a926e5edf
Peter Bacon Darwin <pete@bacondarwin.com>
2013-12-28 20:23:38 +0100

    refact(doc-processor): major refactoring of doc processing

--- bin/lib/default-setup.js
+++ bin/lib/default-setup.js
@@ -6,13 +6,9 @@ var docExtractors = require('../../lib/doc-extractor/doc-extractors');
 
 // Processing ngdoc tags in extracted docs
 var ngDocProcessorFactory =  require('../../lib/ngdoc-parser');
-var ngdocTagHandlers = require('../../lib/ngdoc-parser/ngdoc-tags');
-var inlineTagHandlers = require('../../lib/ngdoc-parser/ngdoc-tags/inline');
-var ngdocPlugins = require('../../lib/ngdoc-parser/ngdoc-plugins');
-var parseDoc = ngDocProcessorFactory(ngdocTagHandlers, inlineTagHandlers, ngdocPlugins);
 
 // Doc processor plugins
-var docProcessorPlugins = require('../../lib/doc-processor/doc-processor-plugins/meta-data');
+var docProcessorPlugins = require('../../lib/doc-processor/doc-processor-plugins');
 
 // Doc processing
 var docParserFactory = require('../../lib/doc-processor');
