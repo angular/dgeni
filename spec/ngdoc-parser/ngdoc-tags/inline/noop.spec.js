fa72ef761a034addc2a7ad47019127c138583f6a
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-01 08:47:01 +0100

    chore(ngdoc-parser): remove ngdoc - moving link plugin to legacy for later

--- spec/ngdoc-parser/ngdoc-tags/inline/noop.spec.js
+++
@@ -1,7 +0,0 @@
-var inlineTag = require('../../../../lib/ngdoc-parser/ngdoc-tags/inline/noop');
-
-describe("noop ngdoc inline tag", function() {
-  it("should just return the original tag", function() {
-    expect(inlineTag({}, 'unknown', 'other information in the tag')).toEqual('{@unknown other information in the tag}');
-  });
-});
