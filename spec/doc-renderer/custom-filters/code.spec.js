ae5cd5e49fda871713661c4b2ae21c06b005c44f
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-02 22:22:41 +0100

    test(doc-renderer): add code tag spec

--- spec/doc-renderer/custom-filters/code.spec.js
+++ spec/doc-renderer/custom-filters/code.spec.js
@@ -4,15 +4,17 @@ var filter = rewire('../../../lib/doc-renderer/custom-filters/code');
 describe("code custom filter", function() {
   var markedMock;
 
-  beforeEach(function() {
-    markedMock = jasmine.createSpy('marked').andReturn('<code>bar</code>');
-    filter.__set__('marked', markedMock);
-  });
   it("should have the name 'code'", function() {
     expect(filter.name).toEqual('code');
   });
-  it("should transform the content using the provided marked function, wrapped in back-ticks", function() {
-    expect(filter.process('foo')).toEqual('<code>bar</code>');
-    expect(markedMock).toHaveBeenCalledWith('`foo`');
+  it("should transform the content using the provided marked function into highlighted code", function() {
+    expect(filter.process('function foo() { }')).toEqual(
+      '<code class="prettyprint linenum">'+
+        '<span class="function">' +
+          '<span class="keyword">function</span> '+
+          '<span class="title">foo</span>' +
+          '<span class="params">()</span> {' +
+        '</span> }' +
+      '</code>');
   });
 });
