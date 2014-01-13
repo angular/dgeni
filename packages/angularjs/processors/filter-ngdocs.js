b22246c06fa26373923d1a24d31eb0166393e794
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-13 22:33:25 +0000

    chore(filter-ngdocs): add logging

--- packages/angularjs/processors/filter-ngdocs.js
+++ packages/angularjs/processors/filter-ngdocs.js
@@ -1,10 +1,14 @@
 var _ = require('lodash');
+var log = require('winston');
 
 module.exports = {
   name: 'filter-ngdocs',
-  before: function(docs, tagParser) {
-    return _.filter(docs, function(doc) {
+  before: function(docs) {
+    var docCount = docs.length;
+    docs = _.filter(docs, function(doc) {
       return doc.tags.getTag('ngdoc');
     });
+    log.debug('filtered ' + (docCount - docs.length) + ' docs');
+    return docs;
   }
 };
