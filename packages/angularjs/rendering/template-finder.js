c270797b6266ed1fc693242ba9dca61c72639454
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-13 22:35:58 +0000

    refact(template-finder): takes a simple config parameter

--- packages/angularjs/rendering/template-finder.js
+++ packages/angularjs/rendering/template-finder.js
@@ -2,14 +2,20 @@ var fs = require('fs');
 var _ = require('lodash');
 var log = require('winston');
 
-module.exports = function templateFinderFactory(templateFolder, extension) {
+module.exports = function templateFinder(config) {
 
-  log.debug('templateFinderFactory', templateFolder, extension);
+  var templateFolder = config.rendering.templatePath;
+  var templateExtension = config.rendering.templateExtension;
+
+  log.debug('templateFinder', templateFolder, templateExtension);
 
   var templateFolderPaths = fs.readdirSync(templateFolder);
   var templateMap = _.indexBy(templateFolderPaths);
         
-  return function templateFinder(doc) {
-    return templateMap[doc.id + '.' + extension] || templateMap[doc.docType + '.' + extension];
+  return function findTemplate(doc) {
+    // Find the template by doc.id, then fall back to by doc.docType
+    // e.g. ngAnimate.module.templates.html -> module.templates.html
+    return templateMap[doc.id + '.' + doc.docType + '.' + templateExtension] ||
+           templateMap[doc.docType + '.' + templateExtension];
   };
 };
