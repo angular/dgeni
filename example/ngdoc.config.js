0b888df04f43c895934db864dc0310a1f2dfb4fc
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-13 22:34:47 +0000

    feat(ngdoc.config.js): added config for example

---
+++ example/ngdoc.config.js
@@ -0,0 +1,22 @@
+var angularjsPackage = require('../packages/angularjs');
+
+module.exports = function(config) {
+  
+  config = angularjsPackage(config);
+  
+  config.source.files = [
+    'src/**/*.js',
+    { pattern: 'content/**/*.ngdoc', basePath: 'content' }
+  ];
+
+  config.processing.dumpToFile = 'docs.txt';
+
+  config.rendering.outputPath = 'build';
+  config.rendering.extra = {
+    git: {
+      tag: "v1.2.6-build.1989+sha.b0474cb"
+    }
+  };
+
+  return config;
+};
