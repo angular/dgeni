8b7f49342cdfe85a8dd705f9f28a8dd078846fab
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-14 07:20:05 +0000

    chore(template-finder): add logging

--- packages/angularjs/rendering/template-finder.js
+++ packages/angularjs/rendering/template-finder.js
@@ -12,7 +12,7 @@ module.exports = function templateFinder(config) {
       '    templateExtension: "..."\n' +
       '  }\n' +
       '}');
-  } 
+  }
 
   var templateFolder = config.rendering.templatePath;
   var templateExtension = config.rendering.templateExtension;
@@ -21,12 +21,19 @@ module.exports = function templateFinder(config) {
 
   var templateFolderPaths = fs.readdirSync(templateFolder);
   var templateMap = _.indexBy(templateFolderPaths);
+
+  log.debug('templates found', templateMap);
         
   return function findTemplate(doc) {
     // Find the template by doc.id, then fall back to by doc.docType
     // e.g. ngAnimate.module.templates.html -> module.templates.html
-    return templateMap[doc.id + '.' + doc.docType + '.' + templateExtension] ||
+    var template = templateMap[doc.id + '.' + doc.docType + '.' + templateExtension] ||
            templateMap[doc.id + '.' + templateExtension] ||
            templateMap[doc.docType + '.' + templateExtension];
+    if(!template) {
+      throw new Error('Unable to find a template for doc:', doc.id, doc.docType);
+    }
+    log.debug('found template: ', template);
+    return template;
   };
 };
