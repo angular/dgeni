f4fd889256580e111784c126fe95616af4210489
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-17 09:29:09 +0000

    feat(examples processor): extract file content from examples

--- packages/angularjs/processors/examples.js
+++ packages/angularjs/processors/examples.js
@@ -1,9 +1,9 @@
 var _ = require('lodash');
 var log = require('winston');
 var walk = require('../../../lib/utils/walk');
-var EXAMPLE_REGEX = /<doc:example([^>]*)>([\S\s]+?)<\/doc:example>/g;
+var EXAMPLE_REGEX = /<example([^>]*)>([\S\s]+?)<\/example>/g;
 var ATTRIBUTE_REGEX = /\s*([^=]+)\s*=\s*(?:(?:"([^"]+)")|(?:'([^']+)'))/g;
-var FILE_REGEX = /<doc:file([^>]*)>([\S\s]+?)<\/doc:file>/g;
+var FILE_REGEX = /<file([^>]*)>([\S\s]+?)<\/file>/g;
 
 // A holder for all the examples that have been found in the document
 var examples;
@@ -19,24 +19,21 @@ module.exports = {
     function extractAttributes(attributeText) {
       var attributes = {};
       attributeText.replace(ATTRIBUTE_REGEX, function(match, prop, val1, val2){
-        log.info(match, prop, val1, val2);
         attributes[prop] = val1 || val2;
       });
       return attributes;
     }
 
     function extractFiles(exampleText) {
-      var matches = exampleText.match(FILE_REGEX);
       var files = {};
-      _.forEach(matches, function(match) {
-        
-        var attributes = extractAttributes(match[1]);
+      exampleText.replace(FILE_REGEX, function(match, attributesText, contents) {
+        var attributes = extractAttributes(attributesText);
         if ( !attributes.name ) {
           throw new Error('Missing name attribute in example for document "' + doc.id + '"');
         }
 
-        // Extract the cotents of the file
-        attributes.fileContent = match[2];
+        // Extract the contents of the file
+        attributes.fileContents = contents;
 
         // Store this file information
         files[attributes.name] = attributes;
@@ -45,29 +42,25 @@ module.exports = {
     }
 
     function processExample(match, attributeText, exampleText) {
-      var attributes = extractAttributes(attributeText);
-      attributes.files = extractFiles(exampleText);
-      attributes.id = examples.length;
+      var example = extractAttributes(attributeText);
+      example.files = extractFiles(exampleText);
+      example.id = examples.length;
       
       // store the example information for later
-      examples.push(attributes);
-
-      // replace with an angular directive that can connect to the stored example
-      // i.e. via id
+      examples.push(example);
     }
 
     // Walk the properties of the document and parse the examples
     walk(doc, function(property, key) {
       if ( _.isString(property) ) {
-        return property.replace(EXAMPLE_REGEX, processExample);
-      } else {
-        return property;
+        property.replace(EXAMPLE_REGEX, processExample);
       }
+      return property;
     });
   },
 
   after: function(docs) {
     // Create new documents (which will be rendered as javascript) for each of the examples
-    log.info(examples);
+    log.info(require('util').inspect(examples, { depth: 4 }));
   }
 };
