e5a5440ad87285222b2b3ec4ca75a3cbe5d7b4d4
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-20 22:33:04 +0000

    fix(paths processor): rename and fix output path property

--- packages/angularjs/processors/paths.js
+++ packages/angularjs/processors/paths.js
@@ -2,10 +2,11 @@ var path = require('canonical-path');
 var checkProperty = require('../../../lib/utils/check-property');
 var codeName = require('../../../lib/utils/code-name');
 
-// This plugin will compute the path to the partial that will be generated
-// from the code name or original file path of the doc.
+// 
 module.exports = {
-  name: 'compute-path',
+  name: 'paths',
+  description: 'This plugin will compute the path to the route and the output file for the partial' +
+               'that will be generated from the code name or original file path of the doc.',
   each: function(doc) {
     if ( doc.fileType === 'js' ) {
 
@@ -16,13 +17,14 @@ module.exports = {
 
       doc.path = codeName.getCodePath(doc, parts);
 
+      // Strip off any hash fragment...
       // If the parts don't contain a "name" then it is a module, in which case use index.html
-      doc.filePath = doc.path + (parts.name ? '.html' : '/index.html');
+      doc.outputPath = doc.path.split('#').shift() + (parts.name ? '.html' : '/index.html');
 
     } else {
       // Replace the original extension with .html
       doc.path = path.dirname(doc.file) + '/' + path.basename(doc.file, '.' + doc.fileType);
-      doc.filePath = doc.path + '.html';
+      doc.outputPath = doc.path + '.html';
     }
   }
 };
--- packages/angularjs/spec/processors/paths.spec.js
+++ packages/angularjs/spec/processors/paths.spec.js
@@ -1,14 +1,16 @@
-var plugin = require('../../processors/path');
-describe("path doc processor plugin", function() {
+var plugin = require('../../processors/paths');
+describe("paths doc processor plugin", function() {
   it("should compute the path from the document section and id if it is a js file", function() {
     var doc = { fileType: 'js', section: 'api', id: 'module:ng.directive:input[checkbox]#someMethod'};
     plugin.each(doc);
     expect(doc.path).toEqual('api/ng/directive/input[checkbox]#someMethod');
+    expect(doc.outputPath).toEqual('api/ng/directive/input[checkbox].html');
   });
 
   it("should compute the path from the section file name and file type", function() {
     var doc = { fileType: 'ngdoc', file: 'guide/directives.ngdoc'};
     plugin.each(doc);
     expect(doc.path).toEqual('guide/directives');
+    expect(doc.outputPath).toEqual('guide/directives.html');
   });
 });
