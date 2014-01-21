000f082475eb7ea36396475e16dda6f4ed5ba1da
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-21 13:03:48 +0000

    feat(examples processor): create docs from examples

--- packages/angularjs/processors/examples.js
+++ packages/angularjs/processors/examples.js
@@ -1,12 +1,13 @@
 var _ = require('lodash');
 var log = require('winston');
+var path = require('canonical-path');
 var walk = require('../../../lib/utils/walk');
 var EXAMPLE_REGEX = /<example([^>]*)>([\S\s]+?)<\/example>/g;
 var ATTRIBUTE_REGEX = /\s*([^=]+)\s*=\s*(?:(?:"([^"]+)")|(?:'([^']+)'))/g;
 var FILE_REGEX = /<file([^>]*)>([\S\s]+?)<\/file>/g;
 
 // A holder for all the examples that have been found in the document
-var examples;
+var examples, exampleNames;
 
 function extractAttributes(attributeText) {
   var attributes = {};
@@ -33,13 +34,16 @@ function extractFiles(exampleText) {
   return files;
 }
 
-function processExample(match, attributeText, exampleText) {
-  var example = extractAttributes(attributeText);
-  example.files = extractFiles(exampleText);
-  example.id = (example.name || 'example') + '-' + examples.length;
-  
-  // store the example information for later
-  examples.push(example);
+function uniqueName(name) {
+  if ( exampleNames[name] ) {
+    var index = 1;
+    while(exampleNames[name + index]) {
+      index += 1;
+    }
+    name = name + index;
+  }
+  exampleNames[name] = true;
+  return name;
 }
 
 module.exports = {
@@ -48,8 +52,19 @@ module.exports = {
   requires: ['doctrine-tag-extractor'],
   init: function(config) {
     examples = [];
+    exampleNames = {};
   },
   each: function(doc) {
+
+    function processExample(match, attributeText, exampleText) {
+      var example = extractAttributes(attributeText);
+      example.files = extractFiles(exampleText);
+      example.id = uniqueName(example.name || 'example');
+      example.doc = doc;
+      
+      // store the example information for later
+      examples.push(example);
+    }
 
     // Walk the properties of the document and parse the examples
     walk(doc, function(property, key) {
@@ -61,7 +76,32 @@ module.exports = {
   },
 
   after: function(docs) {
-    // Create new documents (which will be rendered as javascript) for each of the examples
-    //log.info(require('util').inspect(examples, { depth: 4 }));
+    _.forEach(examples, function(example) {
+      var outputFolder = path.join('examples', example.id);
+      
+      var exampleDoc = {
+        docType: 'example',
+        template: 'example/index.template.html',
+        file: example.doc.file,
+        startingLine: example.doc.startingLine,
+        example: example,
+        path: outputFolder,
+        outputPath: path.join(outputFolder, 'index.html')
+      };
+      docs.push(exampleDoc);
+
+      _.forEach(example.files, function(file) {
+        var fileDoc = {
+          docType: 'example-' + (file.type || 'file'),
+          template: 'example/' + (file.type || 'file') + '.template.txt',
+          file: example.doc.file,
+          startingLine: example.doc.startingLine,
+          example: example,
+          path: path.join(outputFolder, file.name),
+          outputPath: path.join(outputFolder, file.name)
+        };
+        docs.push(fileDoc);
+      });
+    });
   }
 };
--- packages/angularjs/spec/processors/examples.spec.js
+++ packages/angularjs/spec/processors/examples.spec.js
@@ -17,32 +17,64 @@ describe("examples doc processor", function() {
 
   it("should extract example tags from the doc content", function() {
     plugin.each({
-      someProp: 'a b c <example name="bar1" moo1="nar1">some example content 1</example> x y z\n' +
-                'a b c <example name="bar2" moo2="nar2">some example content 2</example> x y z',
+      someProp: 'a b c <example name="bar" moo1="nar1">some example content 1</example> x y z\n' +
+                'a b c <example name="bar" moo2="nar2">some example content 2</example> x y z',
       otherProp: 'j k l \n<example name="value">some content \n with newlines</example> j k l',
       exampleWithFiles: '<example name="example-with-files"><file name="app.js">aaa</file><file name="app.spec.js">bbb</file></example>'
     });
-    expect(plugin.__get__('examples')).toEqual([
-      { name:'bar1', moo1:'nar1', files: {} , id: 'bar1-0'},
-      { name:'bar2', moo2:'nar2', files: {} , id: 'bar2-1'},
-      { name:'value', files: {}, id: 'value-2' },
-      { name: 'example-with-files', files: {
+    var examples = plugin.__get__('examples');
+    expect(examples[0]).toEqual(jasmine.objectContaining({ name:'bar', moo1:'nar1', files: {} , id: 'bar'}));
+    expect(examples[1]).toEqual(jasmine.objectContaining({ name:'bar', moo2:'nar2', files: {} , id: 'bar1'}));
+    expect(examples[2]).toEqual(jasmine.objectContaining({ name:'value', files: {}, id: 'value'}));
+    expect(examples[3]).toEqual(jasmine.objectContaining({
+      name: 'example-with-files',
+      files: {
         'app.js': { name: 'app.js', fileContents: 'aaa' },
         'app.spec.js': { name: 'app.spec.js', fileContents: 'bbb' },
-      }, id: 'example-with-files-3'}
-    ]);
+      },
+      id: 'example-with-files'
+    }));
+  });
+
+
+  it("should compute unique ids for each example", function() {
+    plugin.each({
+      fileContents: '<example name="bar">some example content 1</example>\n' +
+                    '<example name="bar">some example content 2</example>'
+    });
+    var examples = plugin.__get__('examples');
+    expect(examples[0].id).toEqual('bar');
+    expect(examples[1].id).toEqual('bar1');
   });
 
 
   it("should add new documents that represent the examples", function() {
     var docs = [{
+      docType: 'directive',
       content:
         '<example name="example-a">\n' +
-          '<file type="js" name="app.js">function() { dooStuff(); }</file>\n' +
+          '<file name="app.js">function() { dooStuff(); }</file>\n' +
+          '<file name="app.css">div.red: { color: red; }</file>\n' +
+          '<file type="spec" name="appSpec.js">describe("some thing", function() { ... });</file>\n' +
         '</example>'
     }];
     plugin.init();
     plugin.each(docs[0]);
     docs = plugin.after(docs) || docs;
+    expect(docs[0]).toEqual(
+      jasmine.objectContaining({ docType: 'directive' })
+    );
+    expect(docs[1]).toEqual(
+      jasmine.objectContaining({ docType: 'example' })
+    );
+    expect(docs[2]).toEqual(
+      jasmine.objectContaining({ docType: 'example-file' })
+    );
+    expect(docs[3]).toEqual(
+      jasmine.objectContaining({ docType: 'example-file' })
+    );
+    expect(docs[4]).toEqual(
+      jasmine.objectContaining({ docType: 'example-spec' })
+    );
   });
 });
