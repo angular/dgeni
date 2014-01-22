96c87877f74b2c9328492111618a8d68ba905e93
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-22 15:03:22 +0000

    chore(example): initial gulp configuration

---
+++ example/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var rimraf = require('gulp-rimraf');
+var loadConfig = require('../lib/utils/config');
+var docGenerator = require('../lib/index');
+
+gulp.task('clean', function() {
+  // We indicate to gulp that this task is async by returning
+  // the stream - gulp can then wait for the stream to close before
+  // starting dependent tasks - see 'default' task below
+  return gulp.src('build', { read: false })
+  .pipe(rimraf());
+});
+
+gulp.task('assets', function() {
+  gulp.src(['assets/**/*'])
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('doc-gen', function() {
+  var config = loadConfig('ngdoc.config.js');
+  return docGenerator(config).generateDocs();
+});
+
+gulp.task('default', ['clean'], function() {
+  return gulp.run('assets', 'doc-gen');
+});
