122abdb62377e85097db70621747aac0f6e3dd78
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-23 12:42:17 +0000

    chore(example): copy bower components into build folder

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var rimraf = require('gulp-rimraf');
 var bower = require('bower');
 var docGenerator = require('../lib/index');
+var merge = require('event-stream').merge;
 
 gulp.task('clean', function() {
   // We indicate to gulp that this task is async by returning
@@ -11,9 +12,21 @@ gulp.task('clean', function() {
   .pipe(rimraf());
 });
 
-gulp.task('assets', ['clean'], function() {
-  return gulp.src(['assets/**/*'])
-  .pipe(gulp.dest('build'));
+gulp.task('bower', function() {
+  return bower.commands.install();
+});
+
+gulp.task('assets', ['bower', 'clean'], function() {
+  return merge(
+    gulp.src(['assets/**/*']).pipe(gulp.dest('build')),
+    gulp.src('bower_components/components-font-awesome/css/**').pipe(gulp.dest('build/components/font-awesome/css')),
+    gulp.src('bower_components/components-font-awesome/font/**').pipe(gulp.dest('build/components/font-awesome/font')),
+    gulp.src('bower_components/bootstrap/**').pipe(gulp.dest('build/components/bootstrap')),
+    gulp.src('bower_components/lunr.js/lunr.js').pipe(gulp.dest('build/components')),
+    gulp.src('bower_components/lunr.js/lunr.min.js').pipe(gulp.dest('build/components')),
+    gulp.src('bower_components/query/jquery.js').pipe(gulp.dest('build/components')),
+    gulp.src('bower_components/query/jquery.min.js').pipe(gulp.dest('build/components'))
+  );
 });
 
 gulp.task('doc-gen', ['clean'], function() {
