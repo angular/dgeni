18de60b0d8bce8e389950d120f623fdab1b20864
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-23 14:03:57 +0000

    chore(example config): move deployments to example

--- example/ngdoc.config.js
+++ example/ngdoc.config.js
@@ -20,6 +20,35 @@ module.exports = function(config) {
   };
 
   config.logging.level = 'info';
+  
+
+  config.deployment = {
+    environments: [{
+      name: 'debug',
+      scripts: [
+        'angular.js',
+        'angular-resource.js',
+        'angular-route.js',
+        'angular-cookies.js',
+        'angular-sanitize.js',
+        'angular-touch.js',
+        'angular-animate.js',
+        'js/angular-bootstrap/bootstrap.js',
+        'js/angular-bootstrap/bootstrap-prettify.js',
+        //'js/google-code-prettify.js',
+        'components/lunr.js/lunr.js',
+        'docs-data.js',
+        'js/docs.js'
+      ],
+      stylesheets: [
+        'components/bootstrap/css/bootstrap.css',
+        'components/font-awesome/css/font-awesome.css',
+        //'css/prettify.css',
+        'css/docs.css',
+        'css/animations.css'
+      ]
+    }]
+  };
 
   return config;
 };
