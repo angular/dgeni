b4a0c334eeda129b72ee5cadad738dd7af468320
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-24 21:57:29 +0000

    feat(example): add gulp server task to run the generated web app

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var rimraf = require('gulp-rimraf');
 var bower = require('bower');
 var docGenerator = require('../lib/index');
 var merge = require('event-stream').merge;
+var express = require('express');
+
+
+// We indicate to gulp that tasks are async by returning the stream.
+// Gulp can then wait for the stream to close before starting dependent tasks.
+// See clean and bower for async tasks, and see assets and doc-gen for dependent tasks below
 
 gulp.task('clean', function() {
-  // We indicate to gulp that this task is async by returning
-  // the stream - gulp can then wait for the stream to close before
-  // starting dependent tasks - see 'default' task below
   return gulp.src('build', { read: false })
   .pipe(rimraf());
 });
@@ -27,12 +31,47 @@ gulp.task('assets', ['bower', 'clean'], function() {
   );
 });
 
+
 gulp.task('doc-gen', ['clean'], function() {
   return docGenerator('ngdoc.config.js').generateDocs();
 });
 
+
+// The default task that will be run if no task is supplied
 gulp.task('default', ['assets', 'doc-gen']);
 
+
+// Watch the files and run the default task when something changes
 gulp.task('watch', function() {
-  gulp.watch(['assets/**', 'content/**', 'src/**', 'ngdoc.config.js'], ['default']);
+  return gulp.watch([
+    'assets/**/*',
+    'content/**/*',
+    'src/**/*',
+    'ngdoc.config.js',
+    '../lib/**/*',
+    '../packages/**/*'
+  ], function() {
+    // I couldn't providing an array of tasks to run to work so had to go with a callback and gulp.run()
+    return gulp.run('default');
+  });
+});
+
+
+// A simple server that can cope with HTML5 deep links
+gulp.task('server', function() {
+  var port = 8000;
+  var build = gulp.env.type || '';
+  var indexPage = ['index',build].join('-') + '.html';
+  var app = express();
+  // Log requests to the console
+  app.use(express.logger());
+  // If the file exists then supply it
+  app.use(express.static('build'));
+  // Otherwise just send the index.html for other files to support HTML5Mode
+  app.all('/*', function(req, res) {
+    res.sendfile(indexPage, { root: 'build' });
+  });
+  app.listen(port, function() {
+    gutil.log('Server listening on port', gutil.colors.magenta(port));
+  });
 });
