0bee9d1cdbbf170b6bd92d674cc7143631fecc46
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-25 09:49:50 +0000

    fix(path processor): fix spec since outputPath now points to partials

--- packages/angularjs/spec/processors/paths.spec.js
+++ packages/angularjs/spec/processors/paths.spec.js
@@ -4,13 +4,13 @@ describe("paths doc processor plugin", function() {
     var doc = { basePath: 'src', fileType: 'js', section: 'api', id: 'module:ng.directive:input[checkbox]#someMethod'};
     plugin.each(doc);
     expect(doc.path).toEqual('api/ng/directive/input[checkbox]#someMethod');
-    expect(doc.outputPath).toEqual('api/ng/directive/input[checkbox].html');
+    expect(doc.outputPath).toEqual('partials/api/ng/directive/input[checkbox].html');
   });
 
   it("should compute the path from the section file name and file type", function() {
     var doc = { fileType: 'ngdoc', file: 'guide/directives.ngdoc', basePath: 'content' };
     plugin.each(doc);
     expect(doc.path).toEqual('guide/directives');
-    expect(doc.outputPath).toEqual('guide/directives.html');
+    expect(doc.outputPath).toEqual('partials/guide/directives.html');
   });
 });
