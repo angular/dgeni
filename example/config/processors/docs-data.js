9920bea0bf1f8766edceb8eec5fa3d7d3e88baaa
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-27 19:40:37 +0000

    feat(docs-data processor): fix up the version picker

--- example/config/processors/docs-data.js
+++ example/config/processors/docs-data.js
@@ -3,84 +3,44 @@ var shell = require('shelljs');
 var fs = require('fs');
 
 function sortVersionsNaturally(versions) {
-  var versionMap = {},
-      NON_RC_RELEASE_NUMBER = 999;
-  for(var i = versions.length - 1; i >= 0; i--) {
-    var version = versions[i];
-    var split = version.split(/\.|rc/);
-     var baseVersion = split[0] + '.' + split[1] + '.' + split[2];
+  var pad = function(num) { return ('0000'+num).slice(-4); };
+  var VERSION_REGEX = /(\d+)\.(\d+)\.(\d+)(?:-?rc\.?(\d+))?/;
 
-    //create a map of RC versions for each version
-    //this way each RC version can be sorted in "natural" order
-    versionMap[baseVersion] = versionMap[baseVersion] || [];
-
-    //NON_RC_RELEASE_NUMBER is used to signal the non-RC version for the release and
-    //it will always appear at the top of the list since the number is so high!
-    versionMap[baseVersion].push(
-      version == baseVersion ? NON_RC_RELEASE_NUMBER : parseInt(version.match(/rc\.?(\d+)/)[1], 10));
-  }
-
-  //flatten the map so that the RC versions occur in a natural sorted order
-  //and the official non-RC version shows up at the top of the list of sorted
-  //RC versions!
-  var angularVersions = [];
-  sortedKeys(versionMap).forEach(function(key) {
-    var versions = versionMap[key];
-
-    //basic numerical sort
-    versions.sort(function(a,b) {
-      return a - b;
-    });
-
-    versions.forEach(function(v) {
-      angularVersions.push(v == NON_RC_RELEASE_NUMBER ? key : key + 'rc' + v);
-    });
+  return _.sortBy(versions, function(version) {
+    var matches = VERSION_REGEX.exec(version);
+    // We pad out the version numbers with 0s so they sort nicely
+    // Non-Release Candidates get 0999 for the last section to make them appear
+    // earlier than release candidates
+    var versionPadded = pad(matches[1]) + pad(matches[2]) + pad(matches[3]) + pad(matches[4] || 999);
+    return versionPadded;
   });
-
-  return angularVersions;
 }
 
-function sortedKeys(obj) {
-  var keys = [];
-  for(var key in obj) {
-    keys.push(key);
-  }
-  keys.sort(true);
-  return keys;
-}
 
 function expandVersions(versions, latestVersion) {
-  var RC_VERSION = /rc\d/;
-  //copy the array to avoid changing the versions param data
-  //the latest version is not on the git tags list, but
-  //docs.angularjs.org will always point to master as of 1.2
+  // The version is stable if its 2nd number is even and doesn't contain "rc"
+  var UNSTABLE_REGEX = /\d+\.\d*[13579]\.\d+|rc/;
+
+  //copy the array to avoid changing the versions param data the latest version is not on the
+  //git tags list, but docs.angularjs.org will always point to master as of 1.2
   versions = versions.concat([latestVersion]);
 
-  var firstUnstable, expanded = [];
-  for(var i=versions.length-1;i>=0;i--) {
-    var version = versions[i],
-        split = version.split('.'),
-        isMaster = version == latestVersion,
-        isStable = split[1] % 2 === 0 && !RC_VERSION.test(version);
-
+  return _.map(versions, function(version) {
+    var isMaster = version === latestVersion;
+    var isStable = !UNSTABLE_REGEX.test(version);
     var title = 'AngularJS - v' + version;
-
-    var docsPath = version < '1.0.2' ?  'docs-' + version : 'docs';
-
+    var docsPath = version < 'v1.0.2' ? 'docs-' + version : 'docs';
     var url = isMaster ?
       'http://docs.angularjs.org' :
       'http://code.angularjs.org/' + version + '/' + docsPath;
-
-    expanded.push({
-      version : version,
-      stable : isStable,
-      title : title,
-      group : (isStable ? 'Stable' : 'Unstable'),
-      url : url
-    });
-  }
-
-  return expanded;
+    return {
+      version: version,
+      stable: isStable,
+      group: isStable ? 'Stable' : 'Unstable',
+      title: title,
+      url: url
+    };
+  }).reverse();
 }
 
 var version;
@@ -117,14 +77,24 @@ function ngCurrentVersion() {
 }
 
 function ngVersions() {
-  var versions = [], regex = /^v([1-9]\d*(?:\.\d+\S+)+)$/; //only fetch >= 1.0.0 versions
-  shell.exec('git tag', {silent: true}).output.split(/\s*\n\s*/)
-    .forEach(function(line) {
-      var matches = regex.exec(line);
-      if(matches && matches.length > 0) {
-        versions.push(matches[1]);
-      }
-    });
+  var regex = /^v([1-9]\d*(?:\.\d+\S+)+)$/; //only fetch >= 1.0.0 versions
+  var versions = shell.exec('git tag', {silent: true}).output.split(/\s*\n\s*/);
+
+  versions = [
+    'g3-v1.0.0-rc2', 'g3-v1.0.0rc1', 'v0.10.0', 'v0.10.1', 'v0.10.2', 'v0.10.3', 'v0.10.4', 'v0.10.5', 'v0.10.6', 'v0.9.0', 'v0.9.1', 'v0.9.10',
+    'v0.9.11', 'v0.9.12', 'v0.9.13', 'v0.9.14', 'v0.9.15', 'v0.9.16', 'v0.9.17', 'v0.9.18', 'v0.9.19', 'v0.9.2', 'v0.9.3',
+    'v0.9.4', 'v0.9.5', 'v0.9.6', 'v0.9.7', 'v0.9.9', 'v1.0.0', 'v1.0.0rc1', 'v1.0.0rc10', 'v1.0.0rc11', 'v1.0.0rc12', 'v1.0.0rc2', 'v1.0.0rc3', 'v1.0.0rc4', 'v1.0.0rc5',
+    'v1.0.0rc6', 'v1.0.0rc7', 'v1.0.0rc8', 'v1.0.0rc9', 'v1.0.1', 'v1.0.2', 'v1.0.3', 'v1.0.4', 'v1.0.5', 'v1.0.6', 'v1.0.7', 'v1.0.8',
+    'v1.1.0', 'v1.1.1', 'v1.1.2', 'v1.1.3', 'v1.1.4', 'v1.1.5', 'v1.2.0', 'v1.2.0-rc.2', 'v1.2.0-rc.3', 'v1.2.0rc1', 'v1.2.1', 'v1.2.10',
+    'v1.2.2', 'v1.2.3', 'v1.2.4', 'v1.2.5', 'v1.2.6', 'v1.2.7', 'v1.2.8', 'v1.2.9'
+  ];
+
+  versions = _.filter(_.map(versions, function(version) {
+    var matches = regex.exec(version);
+    if(matches && matches.length > 0) {
+      return matches[1];
+    }
+  }));
 
   //match the future version of AngularJS that is set in the package.json file
   return expandVersions(sortVersionsNaturally(versions), ngCurrentVersion().full);
