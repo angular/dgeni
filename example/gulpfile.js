f84a497df0ec6cc0b8789a59e9bb59ff27bc8651
Peter Bacon Darwin <pete@bacondarwin.com>
2014-01-27 11:39:38 +0000

    example(bower): use the correct version of bootstrap CSS

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('assets', ['bower', 'clean'], function() {
   return merge(
     gulp.src(['config/assets/**/*']).pipe(gulp.dest('build')),
     gulp.src('bower_components/components-font-awesome/**').pipe(gulp.dest('build/components/font-awesome')),
-    gulp.src('bower_components/bootstrap/dist/**').pipe(gulp.dest('build/components/bootstrap')),
+    gulp.src('bower_components/bootstrap/**').pipe(gulp.dest('build/components/bootstrap')),
     gulp.src('bower_components/lunr.js/*.js').pipe(gulp.dest('build/components/lunr.js')),
     gulp.src('bower_components/jquery/*.js').pipe(gulp.dest('build/components/jquery')),
     gulp.src('node_modules/marked/**/*.js').pipe(gulp.dest('build/components/marked'))
