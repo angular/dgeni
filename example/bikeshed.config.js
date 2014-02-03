251c0fe2fa00d7a3e57798138b3e488208e04c43
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-03 01:13:00 +0000

    feat(example): add default environment

--- example/bikeshed.config.js
+++ example/bikeshed.config.js
@@ -46,6 +46,34 @@ module.exports = function(config) {
         'css/docs.css',
         'css/animations.css'
       ]
+    },
+    {
+      name: 'default',
+      scripts: [
+        'angular.min.js',
+        'angular-resource.min.js',
+        'angular-route.min.js',
+        'angular-cookies.min.js',
+        'angular-sanitize.min.js',
+        'angular-touch.min.js',
+        'angular-animate.min.js',
+        'components/marked/lib/marked.js',
+        'js/angular-bootstrap/bootstrap.js',
+        'js/angular-bootstrap/bootstrap-prettify.js',
+        'components/lunr.js/lunr.min.js',
+        'components/google-code-prettify/src/prettify.js',
+        'components/google-code-prettify/src/lang-css.js',
+        'js/versions-data.js',
+        'js/pages-data.js',
+        'js/docs.js'
+      ],
+      stylesheets: [
+        'css/bootstrap/css/bootstrap.css',
+        'components/open-sans-fontface/open-sans.css',
+        'css/prettify-theme.css',
+        'css/docs.css',
+        'css/animations.css'
+      ]
     }]
   });
 
