fad8f0acb699621d70772087f5a3b72bfa657085
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-17 21:56:59 +0000

    chore(dgeni.conf.js): add a simple dgeni config

---
+++ dgeni.conf.js
@@ -0,0 +1,24 @@
+var _ = require('lodash');
+var path = require('canonical-path');
+var dgeni = require('./lib');
+var jsdocPackage = require('dgeni-packages/jsdoc');
+
+var basePath = path.resolve(__dirname);
+
+module.exports = function(config) {
+  config = jsdocPackage(config);
+
+  config.set('source.projectPath', basePath);
+
+  config.set('source.files', [
+    { pattern: 'lib/**/*.js', basePath: basePath }
+  ]);
+
+  config.set('processing.stopOnError', true);
+  config.set('rendering.outputFolder', 'build');
+  config.set('rendering.contentsFolder', 'docs');
+
+  config.set('logging.level', 'debug');
+
+  return config;
+};
