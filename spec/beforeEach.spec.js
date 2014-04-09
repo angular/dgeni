eae4d11fa161b7e7bc2096e882ebe3aa0fe763f4
Peter Bacon Darwin <pete@bacondarwin.com>
2014-04-09 21:03:54 +0100

    chore(spec/beforeEach): make logging silent
    Spying on the logger was not actually working.  This change just makes
    the console logger silent but actually we should add our own custom
    transport that saves to memory so we can check what is being logged
    as part of the tests.

--- spec/beforeEach.spec.js
+++ spec/beforeEach.spec.js
@@ -1,33 +1,6 @@
 var log = require('winston');
 
-// Apart from providing us with a nice way to programmatically check the logging
-// messages, this also prevents unwanted messages being written to the console.
+log.remove(log.transports.Console);
 
-beforeEach(function() {
-  log._errors = [];
-  spyOn(log, 'error').andCallFake(function() {
-    log._errors.push(arguments);
-  });
 
-  log._warns = [];
-  spyOn(log, 'warn').andCallFake(function() {
-    log._warns.push(arguments);
-  });
-
-  log._infos = [];
-  spyOn(log, 'info').andCallFake(function() {
-    log._infos.push(arguments);
-  });
-
-  log._debugs = [];
-  spyOn(log, 'debug').andCallFake(function() {
-    log._debugs.push(arguments);
-  });
-
-  log._sillys = [];
-  spyOn(log, 'silly').andCallFake(function() {
-    log._sillys.push(arguments);
-  });
-
-  spyOn(log, 'cli');
-});
+//TODO: Add a custom transport that logs to an array
