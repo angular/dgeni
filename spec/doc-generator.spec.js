c4b7e347d7aa7b61a9059a94d1313b04d27b846e
Peter Bacon Darwin <pete@bacondarwin.com>
2014-04-10 22:12:27 +0100

    test(doc-generator): improve coverage

--- spec/doc-generator.spec.js
+++ spec/doc-generator.spec.js
@@ -1,13 +1,34 @@
 var docGenerator = require('../lib/doc-generator');
-var Config = require('../lib/config').Config;
+var configurer = require('../lib/config');
+var Config = configurer.Config;
+
+function createConfig() {
+  return new Config({
+    processing: {
+      processors: []
+    }
+  });
+}
 
 describe("doc-generator", function() {
+
+
   it("should return a generator function", function() {
-    var config = new Config({
-      processing: {
-        processors: []
-      }
-    });
-    var generator = docGenerator(config);
+    var generator = docGenerator(createConfig());
+    expect(generator).toEqual(jasmine.any(Function));
+
+    var docs = [];
+    docs = generator(docs);
   });
+
+  it("should try to load a config file if passed a string", function() {
+    spyOn(configurer, 'load').andReturn(createConfig());
+    docGenerator('someFileName');
+    expect(configurer.load).toHaveBeenCalled();
+  });
+
+  it("should raise an error if config is not a Config", function() {
+    expect(function() { docGenerator({}); }).toThrow();
+  });
+
 });
