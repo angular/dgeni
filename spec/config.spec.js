adc0cba1eb0bc7e297272a03e755d4e232b23485
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-28 22:25:40 +0100

    refact(Config): only use Config methods for accessing properties

--- spec/config.spec.js
+++ spec/config.spec.js
@@ -20,31 +20,31 @@ describe("Config", function() {
 
   it("should have functions for changing properties", function() {
     config.set('logging.level', 'debug');
-    expect(config.logging.level).toEqual('debug');
+    expect(config.get('logging.level')).toEqual('debug');
 
     config.append('source.extractors', 'a');
-    expect(config.source.extractors).toEqual(['a']);
+    expect(config.get('source.extractors')).toEqual(['a']);
 
     config.append('source.extractors', ['x', 'y']);
-    expect(config.source.extractors).toEqual(['a', 'x', 'y']);
+    expect(config.get('source.extractors')).toEqual(['a', 'x', 'y']);
 
     config.append('other.newArray', 'a');
-    expect(config.other.newArray).toEqual(['a']);
+    expect(config.get('other.newArray')).toEqual(['a']);
 
     config.prepend('rendering.templateFolders', ['x', 'y']);
-    expect(config.rendering.templateFolders).toEqual(['x', 'y']);
+    expect(config.get('rendering.templateFolders')).toEqual(['x', 'y']);
 
     config.prepend('rendering.templateFolders', ['a', 'b']);
-    expect(config.rendering.templateFolders).toEqual(['a', 'b', 'x', 'y']);
+    expect(config.get('rendering.templateFolders')).toEqual(['a', 'b', 'x', 'y']);
 
     config.prepend('other.newArray2', 'x');
-    expect(config.other.newArray2).toEqual(['x']);
+    expect(config.get('other.newArray2')).toEqual(['x']);
 
     config.set('some.extra.property', 'value');
-    expect(config.some.extra.property).toEqual('value');
+    expect(config.get('some.extra.property')).toEqual('value');
 
     config.merge('some.extra', { foo: 'bar' });
-    expect(config.some.extra).toEqual({
+    expect(config.get('some.extra')).toEqual({
       property: 'value',
       foo: 'bar'
     });
