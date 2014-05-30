bc4c7b543247f645ba9a7d6b30b5f840fecac97c
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-30 22:40:08 +0100

    fix(doc-generator): ensure package dependencies are used

--- spec/doc-generator.spec.js
+++ spec/doc-generator.spec.js
@@ -25,7 +25,40 @@ describe("DocGenerator", function() {
   });
 
   describe("generate", function() {
+    it("should add services from packages in the correct package dependency order", function(done) {
+      var log = [];
+      docGenerator.package('test1', ['test2'])
+        .service('testValue', function() { return 'test 1'; });
+      docGenerator.package('test2')
+        .service('testValue', function() { return 'test 2'; });
+      docGenerator.package('test3', ['test1', 'test2'])
+        .processor({
+          name: 'test3-processor',
+          process: function(testValue) { log.push(testValue); }
+        });
+      return docGenerator.generate().then(function() {
+        expect(log).toEqual(['test 1']);
+        done();
+      });
+    });
 
+
+    it("should run the config functions in the correct package dependency order", function(done) {
+      var log = [];
+      docGenerator.package('test1', ['test2'])
+        .config(function(config) { config.set('testValue', 1); });
+      docGenerator.package('test2')
+        .config(function(config) { config.set('testValue', 2); });
+      docGenerator.package('test3', ['test1', 'test2'])
+        .processor({
+          name: 'test3-processor',
+          process: function(config) { log.push(config.get('testValue')); }
+        });
+      return docGenerator.generate().then(function() {
+        expect(log).toEqual([1]);
+        done();
+      });
+    });
   });
 
 });
